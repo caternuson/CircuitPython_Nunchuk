fa9dabf65f5f4f0211de5ff5a687cc5b87869578
caternuson <caternuson@gmail.com>
2019-07-20 09:05:26 -0700

    cookiegit add .!

--- examples/nunchuk_mouse.py
+++ examples/nunchuk_mouse.py
@@ -2,20 +2,19 @@ import board
 import nunchuk
 from adafruit_hid.mouse import Mouse
 
-STEP = 5
+THRESHOLD = 10
+
 m = Mouse()
 nc = nunchuk.Nunchuk(board.I2C())
 
 while True:
     x, y = nc.joystick
-    if x < 100:
-        m.move(-STEP, 0, 0)
-    if x > 150:
-        m.move( STEP, 0, 0)
-    if y < 100:
-        m.move( 0, STEP, 0)
-    if y > 150:
-        m.move( 0,-STEP, 0)
+    x = (x - 128)//2
+    y = (128 - y)//2
+    if abs(x) > THRESHOLD:
+        m.move(x, 0, 0)
+    if abs(y) > THRESHOLD:
+        m.move(0, y, 0)
     if nc.button_Z:
         m.click(Mouse.LEFT_BUTTON)
     if nc.button_C:
